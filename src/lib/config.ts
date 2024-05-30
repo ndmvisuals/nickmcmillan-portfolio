e2f2ad1af774e22fd6c01334f26ffbb53d9d3638
ndmvisuals <70617650+ndmvisuals@users.noreply.github.com>
2024-05-29 22:45:09 -0400

    delete svelte version of website on main

--- src/lib/config.ts
+++
@@ -1,6 +0,0 @@
-import { dev } from '$app/environment'
-
-
-export const title = 'Nick McMillan'
-export const description = 'Portfolio'
-export const url = dev ? 'http://localhost:5173/' : 'https://nickmcmillan.com/'
